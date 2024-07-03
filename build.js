af4efb500c04a7296f0687611b23eeb05e7ac5ef
J-Mann-123 <jmann5326@gmail.com>
2024-07-03 22:12:58 +0930

    Added new build script

---
+++ build.js
@@ -0,0 +1,27 @@
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
+
+const buildExpo = () => {
+  console.log('Building Expo app...');
+  execSync('expo build:web', { stdio: 'inherit' });
+};
+
+const buildGatsby = () => {
+  console.log('Building Gatsby app...');
+  execSync('gatsby build', { stdio: 'inherit' });
+};
+
+const combineBuilds = () => {
+  console.log('Combining builds...');
+  fs.ensureDirSync('web-build/gatsby');
+  fs.moveSync('public', 'web-build/gatsby', { overwrite: true });
+};
+
+const runBuilds = async () => {
+  buildExpo();
+  buildGatsby();
+  combineBuilds();
+  console.log('Build process completed.');
+};
+
+runBuilds().catch(console.error);
