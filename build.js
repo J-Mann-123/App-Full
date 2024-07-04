f8f64db203f61c5a8b1888b4860a396143e92b85
J-Mann-123 <jmann5326@gmail.com>
2024-07-04 14:40:18 +0930

    new build script

--- build.js
+++ build.js
@@ -1,33 +1,41 @@
 const { execSync } = require('child_process');
 const fs = require('fs-extra');
 
+const installDependencies = (cwd) => {
+  console.log(`Installing dependencies in ${cwd}...`);
+  execSync('npm install', { stdio: 'inherit', cwd });
+};
+
 const buildExpo = () => {
   console.log('Building Expo app...');
+  installDependencies('The-Optometrists-Companion');
   execSync('npx expo export', { stdio: 'inherit', cwd: 'The-Optometrists-Companion' });
 };
 
 const buildGatsby = () => {
   console.log('Building Gatsby app...');
+  installDependencies('optometrists-companion-web-homepage');
   execSync('gatsby build', { stdio: 'inherit', cwd: 'optometrists-companion-web-homepage' });
 };
 
 const combineBuilds = () => {
   console.log('Combining builds...');
-  // Ensure the directory for the Gatsby build exists
   fs.ensureDirSync('web-build/gatsby');
-  // Move the Gatsby build into the web-build directory
   fs.moveSync('public', 'web-build/gatsby', { overwrite: true });
-  // Create a directory for the Expo app within the Gatsby build
   fs.ensureDirSync('web-build/gatsby/expo-app');
-  // Move the Expo build into the newly created directory
   fs.moveSync('dist', 'web-build/gatsby/expo-app', { overwrite: true });
 };
 
 const runBuilds = async () => {
-  buildExpo();
-  buildGatsby();
-  combineBuilds();
-  console.log('Build process completed.');
+  try {
+    buildExpo();
+    buildGatsby();
+    combineBuilds();
+    console.log('Build process completed.');
+  } catch (error) {
+    console.error('Build process failed:', error);
+    process.exit(1);
+  }
 };
 
 runBuilds().catch(console.error);
